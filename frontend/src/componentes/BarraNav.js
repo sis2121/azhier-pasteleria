a4d3b5221a1bd9b437885f84d0edce9469fe7475
joel <lcjoel20@gmail.com>
2026-07-09 17:49:26 -0400

    Cambiar fondo del menú principal por imagen personalizada10

--- frontend/src/componentes/BarraNav.js
+++ frontend/src/componentes/BarraNav.js
@@ -23,7 +23,7 @@ const BarraNav = () => {
             <img
               src="/images/logopastel.png"
               alt="Azhier Pastelería"
-              className="h-12 w-12 object-contain"
+              className="h-14 w-14 object-contain"
             />
             <span>AZHIER</span>
           </Link>
